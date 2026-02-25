493f653a78bb2e72e782d2eaa56b7d671599f171
Iglesia Desta <iglesiad25@gmail.com>
2026-02-25 11:43:02 +0100

    added process.env render dynamic port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use("/assets", express.static("assets"));
-const port = 3000;
+const port = process.env.PORT || 3000;
 let db;
 
 try {
